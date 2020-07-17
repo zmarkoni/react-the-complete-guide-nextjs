6ff3a2188517c6099fb8754ea3e17f3272fe75c8
Zoran Markovic <zoran.markovic@netcentric.biz>
2020-07-17 16:20:22 +0200

    use components

--- pages/auth/index.js
+++ pages/auth/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
+import User from '../../components/User';
+
+// https://www.udemy.com/course/react-the-complete-guide-incl-redux/learn/lecture/8294734#overview
+// we get codespliting out the box
+// routing
+// just use pages for containers and than we have components folder
 
 const authIndexPage = () => (
 	<div>
 		<h1>The Auth Index Page</h1>
+		<User name="Zoki" age={36} />
 	</div>
 );
 
