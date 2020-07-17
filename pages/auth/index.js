17fa9c643026f85570944113fd23a1f43742c5f3
Zoran Markovic <zoran.markovic@netcentric.biz>
2020-07-17 17:56:43 +0200

    getInitialProps update

--- pages/auth/index.js
+++ pages/auth/index.js
@@ -2,32 +2,18 @@ import React from 'react';
 import User from '../../components/User';
 
 // https://www.udemy.com/course/react-the-complete-guide-incl-redux/learn/lecture/8294734#overview
-// we get codespliting out the box
+// we get code spliting out the box
 // routing
 // just use pages for containers and than we have components folder
 // styling is different
 
-const authIndexPage = (props) => {
+const authIndexPage = () => {
 	return (
 		<div>
 			<h1>The Auth Index Page</h1>
-			<div>Next stars: {props.stars}</div>
 			<User name="Zoki" age={36} />
 		</div>
 	);
-};
-
-{
-	/* https://nextjs.org/docs/api-reference/data-fetching/getInitialProps
-	Initialization of PROPS before page is loaded
-	for fetching data and pass to application before initialization
-	can be used in Class based components as well 
-*/
-}
-authIndexPage.getInitialProps = async (ctx) => {
-	const res = await fetch('https://api.github.com/repos/vercel/next.js');
-	const json = await res.json();
-	return { stars: json.stargazers_count };
 };
 
 export default authIndexPage;
--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ import Router from 'next/router';
 // We link only folders
 import Button from '../components/Button';
 
-const indexPage = () => (
+const indexPage = (props) => (
 	<div>
 		<h1>The main Page</h1>
 		<p>
@@ -14,8 +14,24 @@ const indexPage = () => (
 			</Link>
 		</p>
 		<button onClick={() => Router.push('/auth')}>Go to Auth</button>
+
 		<Button>Error button</Button>
+		<p>Fetch and return number of Stars: </p>
+		<div>Next stars: {props.stars}</div>
 	</div>
 );
 
+{
+	/* https://nextjs.org/docs/api-reference/data-fetching/getInitialProps
+	Initialization of PROPS before page is loaded
+	for fetching data and pass to application before initialization
+	can be used in Class based components as well 
+	can be used in any component
+*/
+}
+indexPage.getInitialProps = async (ctx) => {
+	const res = await fetch('https://api.github.com/repos/vercel/next.js');
+	const json = await res.json();
+	return { stars: json.stargazers_count };
+};
 export default indexPage;
